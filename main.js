8e27359de2c4b2e9b166bf42e355f32008ecb1bf
Mychelle Blake <mychelle.blake@gmail.com>
2015-10-20 16:19:12 -0700

    Adds homework files for normal mode.

---
+++ main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+$("#h21").on("click", "a", function() {
+	$("#moveDownLI1").toggleClass("listParClick");
+});
+
+$("#h22").on("click",function() {
+	$("#moveDownLI2").toggleClass("listParClick");
+});
+
+$("#h23").on("click",function() {
+	$("#moveDownLI3").toggleClass("listParClick");
+});
+
+
+});
